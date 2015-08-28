67a4bc21e1aef6c0244cb547ca4cf575cc3c08db
Dmitry Ignatiev <lovesan.ru at gmail.com>
2015-08-28 22:44:40 +0300

    C++ Blocking queue implementation

--- libmonitorxx/libmonitor.hpp
+++ libmonitorxx/libmonitor.hpp
@@ -2,6 +2,8 @@
 #define __LIBMONITOR_HPP__
 
 #include <exception>
+#include <queue>
+#include <ctime>
 #include "libmonitor.h"
 
 #ifdef _WIN32
@@ -90,6 +92,115 @@ private:
     
     LmMonitor(const LmMonitor& rhs);
     LmMonitor& operator=(const LmMonitor& rhs);
+};
+
+template<typename T, typename C = std::deque<T>>
+class LmQueue
+{
+public:
+    LmQueue() : _cap(-1) { }
+
+    LmQueue(int cap) : _cap(cap) { }
+
+    ~LmQueue() { }
+
+    bool Add(const T& element, int timeout)
+    {
+        LmLock lock = _m.Lock();
+        clock_t cl;
+        if (_cap < 0)
+        {
+            _q.push(element);
+            if (_q.size() == 1)
+            {
+                _m.PulseAll();
+            }
+            return true;
+        }
+        else
+        {
+            if (timeout < 0)
+            {
+                while (_q.size() >= _cap)
+                {
+                    _m.Wait();
+                }
+            }
+            else
+            {
+                cl = clock();
+                while (_q.size() >= _cap)
+                {
+                    if (timeout < 0 || !_m.Wait(timeout))
+                    {
+                        return false;
+                    }
+                    timeout -= ((clock() - cl) / CLOCKS_PER_SEC / 1000);
+                    cl = clock();
+                }
+            }
+            _q.push(element);
+            if (_q.size() == 1)
+            {
+                _m.PulseAll();
+            }
+            return true;
+        }
+    }
+
+    void Add(const T& element)
+    {
+        Add(element, -1);
+    }
+
+    bool Remove(T& element, int timeout)
+    {
+        LmLock lock = _m.Lock();
+        clock_t cl;
+        if (timeout < 0)
+        {
+            while (_q.empty())
+            {
+                _m.Wait();
+            }
+        }
+        else
+        {
+            cl = clock();
+            while (_q.empty())
+            {
+                if (timeout < 0 || !_m.Wait(timeout))
+                {
+                    return false;
+                }
+                timeout -= ((clock() - cl) / CLOCKS_PER_SEC / 1000);
+                cl = clock();
+            }
+        }
+        element = _q.front();
+        _q.pop();
+        if (_q.size() == (_cap - 1))
+        {
+            _m.PulseAll();
+        }
+        return true;
+    }
+
+    void Remove(T& element)
+    {
+        Remove(element, -1);
+    }
+
+    int GetSize()
+    {
+        Lock lock = _m.Lock();
+        return _q.size();
+    }
+
+private:
+    LmMonitor _m;
+    std::queue<T, C> _q;
+    int _cap;
 };
 
 #endif // __LIBMONITOR_HPP__
--- libmonitorxx_test/libmonitorxx_test.cpp
+++ libmonitorxx_test/libmonitorxx_test.cpp
@@ -1,42 +1,31 @@
 #include <cstdlib>
 #include <thread>
 #include <iostream>
-#include <queue>
 #include "libmonitor.hpp"
 
-void lm_test_thread_proc(LmMonitor* mon, std::queue<int> *q)
+void lm_test_thread_proc(LmQueue<int> *q)
 {
     for (int i = 1; i <= 10; ++i)
     {
         std::this_thread::sleep_for(std::chrono::milliseconds(int(rand() * 1.0f / RAND_MAX * 100 + 100)));
-        mon->Enter();
-        q->push(i);
-        std::cout << "Pushed " << i << " to queue." << std::endl;
-        mon->Pulse();
-        mon->Exit();
+        q->Add(i);
     }
 }
 
 int main(void)
 {
-    LmMonitor mon;
-    std::queue<int> q;
-    std::thread t(lm_test_thread_proc, &mon, &q);
+    LmQueue<int> q(3);
+    int x;
+    std::thread t(lm_test_thread_proc, &q);
     try {
         for (int i = 1; i <= 10; ++i)
         {
-            auto lock = mon.Lock();
-            while (q.empty())
-            {
-                mon.Wait();
-            }
-            std::cout << "Got " << q.front() << " from queue." << std::endl;
-            if (q.front() != i)
+            q.Remove(x);
+            if (x != i)
             {
                 std::cout << "Abnormal value recieved" << std::endl;
                 throw LmFailedException();
             }
-            q.pop();
         }
         t.join();
         std::cout << "Test finished" << std::endl;
